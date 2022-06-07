4a9452b0a2b0fb5e4da4e466513c49cf6e667ed2
Odimayo David <odimayodavid7@gmail.com>
2022-06-06 17:18:12 -0700

    c programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,10 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/*
- *
+/**
  * main - prints the last digit of the random
- *
  * number stored in the variable n
- *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
